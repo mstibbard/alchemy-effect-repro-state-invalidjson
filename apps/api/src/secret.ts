ac39e7e4c45a31cb8037079b66e75f4b5f6be7d0
Matthew Stibbard <matthew@stibbard.au>
2026-05-04 10:00:06 +1000

    try alchemy default secretstore name

--- apps/api/src/secret.ts
+++ apps/api/src/secret.ts
@@ -3,7 +3,7 @@ import { adopt } from "alchemy/AdoptPolicy";
 import * as Cloudflare from "alchemy/Cloudflare";
 import { Effect, Redacted } from "effect";
 
-export const Store = Cloudflare.SecretsStore("FlowSecretStore").pipe(adopt(true));
+export const Store = Cloudflare.SecretsStore("StateStoreSecrets").pipe(adopt(true));
 
 export const ExampleSecret = Effect.gen(function* () {
 	const store = yield* Store;
